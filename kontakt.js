736633d63154b2f2858b9fcda6803545d99c1301
yde-emma <145010724+yde-emma@users.noreply.github.com>
2024-06-12 12:31:05 +0200

    Update kontakt.js

--- kontakt.js
+++ kontakt.js
@@ -13,11 +13,12 @@ sporgsmaalListe.forEach((sporgsmaal) => {
 /* -----------------
     KONTAKT FORMS 
 ------------------ */
+// Når brugeren forsøger at indsende formularen starter function(event), som tjekker om felterne er udfyldt eller ej
 document.getElementById('kontakt_form').addEventListener('submit', function(event) {
-    // Sørger for at den ikke indsender formen
+    // Sørger for at den ikke indsender formen eller genindlæser siden
     event.preventDefault();
     
-    // Fjerner alle tidligere fejlbeskeder
+    // Fjerner alle tidligere fejlbeskeder og skjuler dem medmindre isValid = false
     document.querySelectorAll('.fejl').forEach(function(error) {
         error.style.display = 'none';
     });
@@ -28,7 +29,9 @@ document.getElementById('kontakt_form').addEventListener('submit', function(even
     const mail = document.getElementById('mail');
     const besked = document.getElementById('besked');
 
-    // Tjekker om input felterne er tomme
+    // Tjekker om input felterne er tomme, eller kun indeholder mellemrum (det er det trim betyder)
+    // Hvis feltet er tomt vises den tilhørende fejlmeddelelse ved at ændre display til "inline"
+    // DETTE NEDENFOR ER EN BOOLEAN med kun true og false som værdier
     let isValid = true;
 
     if (emne.value.trim() === '') {
@@ -51,9 +54,10 @@ document.getElementById('kontakt_form').addEventListener('submit', function(even
         isValid = false;
     }
 
-    // Hvis alle felter er udfyldt vises MODAL
+    // Hvis alle felter er udfyldt korrekt vises modal ved display:block ved tryk på sendKnap og skjules ved display:none ved tryk på lukKaffeModal
     if (isValid) {
-        //VARIABLER TIL MODAL
+
+    //VARIABLER TIL MODAL
     // Vi finder hele event_send_modal i HTML
     const eventsSendModal = document.getElementById("events_send_modal");
 
@@ -74,6 +78,6 @@ document.getElementById('kontakt_form').addEventListener('submit', function(even
             }
 
             /* FØRSTE FORSØG MED ALERT
-        alert('Juhuuu vi glæder os til at se dig/jer!'); // LAV SJOV*/ 
+        alert('Juhuuu vi glæder os til at se dig/jer!'); */
     }
 });
